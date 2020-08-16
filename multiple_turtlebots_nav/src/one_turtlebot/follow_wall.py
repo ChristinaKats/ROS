c596d06c5e6e99932ee31a680a1eccad9034684b
ChristinaKats <xristinakatsarlinou@gmail.com>
2020-08-16 13:37:23 +0300

    clear code, add comments

--- multiple_turtlebots_nav/src/one_turtlebot/follow_wall.py
+++ multiple_turtlebots_nav/src/one_turtlebot/follow_wall.py
@@ -16,30 +16,25 @@ class WallFollow(): # follow the wall until you return at your initial position
     def __init__(self):
         rospy.on_shutdown(self.shutdown)
         rospy.init_node('WallFollow', anonymous=False)
-        self.distances = []
-        self.my_odom = Odometry
-        # self.init_position = Odometry
 
+        self.distances = [] # laserscan distances
+        self.my_odom = Odometry # my position (odom topic)
         self.roll = self.pitch = self.yaw = 0.0
-
         self.scans = rospy.Subscriber('/scan', LaserScan, self.my_distances)
-            
         self.cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=10)
-
         sub = rospy.Subscriber ('/odom', Odometry, self.get_rotation)
 
         r = rospy.Rate(1)
+        self.right_hand_on_wall() # turn to touch the wall with the right hand
 
-        self.right_hand_on_wall()
-
-    def get_rotation(self, msg):
+    def get_rotation(self, msg): # the orientation of the turtlebot
         self.my_odom = msg
         orientation_q = msg.pose.pose.orientation
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
         (self.roll, self.pitch, self.yaw) = euler_from_quaternion(orientation_list)
 
-    def right_hand_on_wall(self):
-        while len(self.distances) == 0:
+    def right_hand_on_wall(self): # turn to touch the wall with the right hand
+        while len(self.distances) == 0: # no scans registered yet, wait
             rospy.loginfo("sleeping")
             rospy.sleep(1)
 
@@ -64,49 +59,38 @@ class WallFollow(): # follow the wall until you return at your initial position
         return
 
     def turn_until_right_hand_on_wall(self):
-        # rospy.loginfo('turn_until_right_hand_on_wall')
-
-        min_element = min(self.distances)
-        # rospy.loginfo('min == %f', min_element)
-        pos_min_element = self.distances.index(min_element)
+        min_element = min(self.distances) # closest obstacle
+        pos_min_element = self.distances.index(min_element) # relative position of obstacle and turtlebot 
+        
         move_cmd = Twist()
 
         # face the wall
-        while pos_min_element < 310 or pos_min_element > 330:
+        while pos_min_element < 310 or pos_min_element > 330: # turn to face the obstacle
             # turn the turtlebot right  
             move_cmd.linear.x = 0 # do not move 
             move_cmd.angular.z = 0.1 # turn only
             self.cmd_vel.publish(move_cmd)
 
             min_element = min(self.distances)
-            # rospy.loginfo('min == %f', min_element)
             pos_min_element = self.distances.index(min_element)
-            # rospy.loginfo('min pos == %d', pos_min_element)
 
         move_cmd.linear.x = 0 # do not move 
         move_cmd.angular.z = 0 # stop turning
         self.cmd_vel.publish(move_cmd)
 
-        # rospy.loginfo('turn 90')
-
         # turn to touch the wall with the right hand
         self.turn_90_degrees()
 
-        # self.walk_by_the_wall()
-
-        
     def turn_90_degrees(self):
-        # rospy.loginfo('turn 90')
-
         move_cmd = Twist()
         # get the current orientation
         current_orient = math.degrees(self.yaw)
         end_orient = math.degrees(self.yaw)
-        while end_orient - current_orient < 89:
-            # rospy.loginfo('angle == %d', end_orient - current_orient)
+        while end_orient - current_orient <= 89: # turn 90 degreess
             move_cmd.linear.x = 0 # do not move 
             move_cmd.angular.z = 0.1 # turn only
             self.cmd_vel.publish(move_cmd)
+
             end_orient = math.degrees(self.yaw)
 
         move_cmd.linear.x = 0 # do not move     
@@ -114,24 +98,22 @@ class WallFollow(): # follow the wall until you return at your initial position
         self.cmd_vel.publish(move_cmd)
 
     def walk_by_the_wall(self):
-        # rospy.loginfo('walk by the wall')
-
         # if you touch the wall, go straight
         move_cmd = Twist()
         distance_from_init = 0
-        # max_distance_from_init = 0
 
-        # while not rospy.is_shutdown(): # Ctr + c
+        # initial position of the turtlebot when it first touched the wall
         init_position_x = self.my_odom.pose.pose.position.x
         init_position_y = self.my_odom.pose.pose.position.y
 
-        flag = False
+        flag = False # boolean variable indicates if the turtlebot got at least 2m away from the initial position
+        # it is used for the following condition. At first flag == False, which means that turtlebot is at the beggining of its trip. 
+        # when the turtlebot moves away of its initial position, flag = True. While flag == True, if distance < 2, 
+        # it shows that the turtlebot returned close to the initial position, so it is time to stop following the wall  
 
-        while distance_from_init > 2 or flag == False:
-            # rospy.loginfo(self.distances[0])
+        while distance_from_init > 2 or flag == False: # until you return to the initial position, follow the wall  
 
-
-            while self.distances[0] > 1.7 and self.distances[0] < 4 and (math.isnan(self.distances[320]) or self.distances[320] > 1.5):
+            while self.distances[0] > 1.7 and self.distances[0] < 4 and (math.isnan(self.distances[320]) or self.distances[320] > 1.5): #follow wall
                 move_cmd.linear.x = 0.5 # go straight 
                 move_cmd.angular.z = 0 # do not turn 
                 self.cmd_vel.publish(move_cmd)
@@ -143,30 +125,27 @@ class WallFollow(): # follow the wall until you return at your initial position
                 if distance_from_init > 2:
                     flag = True
             
-                rospy.loginfo('distance = %f', distance_from_init)
+                # rospy.loginfo('distance = %f', distance_from_init)
 
                 if distance_from_init <= 2 and flag == True:
                     break
 
+            # if you got to close to the wall, turn left
             if (math.isnan(self.distances[0]) == False and self.distances[0] <= 1.7 and self.distances[0] > 0) or (math.isnan(self.distances[320]) == False and self.distances[320] <= 1.5 and self.distances[320] > 0):
-                # rospy.loginfo('turn left %f, %f', self.distances[0], self.distances[320])
-
                 move_cmd.linear.x = 0 # stop moving 
                 move_cmd.angular.z = 0.1 # turn left 
                 self.cmd_vel.publish(move_cmd)
 
-            elif self.distances[0] > 4:
-                # rospy.loginfo('turn right')
+            elif self.distances[0] > 4: # if you break away from the wall, turn right
                 move_cmd.linear.x = 0 # stop moving 
-                move_cmd.angular.z = -0.1 # turn left 
+                move_cmd.angular.z = -0.1 # turn right 
                 self.cmd_vel.publish(move_cmd)
 
-            elif math.isnan(self.distances[0]) or self.distances[0] < 0:
-                # rospy.loginfo('go strainght and turn right')
+            elif math.isnan(self.distances[0]) or self.distances[0] < 0: # if wall ended, go staight for 2m and turn until you reach the a wall
                 init_pose_x = self.my_odom.pose.pose.position.x
                 init_pose_y = self.my_odom.pose.pose.position.y
                 distance = 0
-                while distance < 2: # go straight for 3 m
+                while distance < 2: # go straight for 2m
                     move_cmd.linear.x = 0.5 # move straight 
                     move_cmd.angular.z = 0.0 # do not turn  
                     self.cmd_vel.publish(move_cmd)
@@ -176,22 +155,23 @@ class WallFollow(): # follow the wall until you return at your initial position
 
                     distance = math.sqrt((curr_pose_x - init_pose_x)*(curr_pose_x - init_pose_x) + (curr_pose_y - init_pose_y)*(curr_pose_y - init_pose_y))
                 
-                while math.isnan(self.distances[0]) or self.distances[0] < 0:
+                while math.isnan(self.distances[0]) or self.distances[0] < 0: # until you find a wall at your right
                     move_cmd.linear.x = 0 # stop moving 
-                    move_cmd.angular.z = -0.1 # turn left 
+                    move_cmd.angular.z = -0.1 # turn right 
                     self.cmd_vel.publish(move_cmd)
 
+            # current position of turtlebot
             curr_position_x = self.my_odom.pose.pose.position.x
             curr_position_y = self.my_odom.pose.pose.position.y
 
+            # distance from the initial position
             distance_from_init = math.sqrt((curr_position_x - init_position_x)*(curr_position_x - init_position_x) + (curr_position_y - init_position_y)*(curr_position_y - init_position_y))
-            if distance_from_init > 2:
+            if distance_from_init > 2: # if the turtlebot moved at least 2m from the initial position
                 flag = True
             
-            rospy.loginfo('distance = %f', distance_from_init)
+            # rospy.loginfo('distance = %f', distance_from_init)
 
 
-    
     def shutdown(self): # stop turtlebot
         rospy.loginfo("Stop TurtleBot")
         move_cmd = Twist()
--- multiple_turtlebots_nav/src/one_turtlebot/go_forward.py
+++ multiple_turtlebots_nav/src/one_turtlebot/go_forward.py
@@ -22,20 +22,17 @@ class GoForward(): # go straight until you reach an odstacle
 		self.cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=10)
 		
 		self.infront_distance = 0 # distance of the obstacle in front of the turtlebot
- 
-		r = rospy.Rate(10)
+		r = rospy.Rate(1)
 
 		# move turtlebot
 		move_cmd = Twist()
-		# move_cmd.linear.x = 0.5 # go forward at 0.5 m/s
-		# move_cmd.angular.z = 0 # go straight only
-
 
 		while not rospy.is_shutdown(): # Ctr + c
 			if self.check_next_pos_scan(): # free space in front of the turtlebot
 				move_cmd.linear.x = 0.5 # go forward at 0.5 m/s
 				move_cmd.angular.z = 0 # go straight only
 				self.cmd_vel.publish(move_cmd)
+
 			else: # obstacle ahead
 				move_cmd.linear.x = 0 # stop moving
 				move_cmd.angular.z = 0 # stop turning
@@ -44,27 +41,20 @@ class GoForward(): # go straight until you reach an odstacle
 
 			r.sleep()
 
-                        
 	def shutdown(self): # stop turtlebot
 		rospy.loginfo("Stop TurtleBot")
 
 		move_cmd = Twist()
 		move_cmd.linear.x = 0 # stop moving
 		move_cmd.angular.z = 0 # stop turning
-
 		self.cmd_vel.publish(move_cmd)
 		rospy.sleep(1)
 
-
 	def check_next_pos_scan(self):
-		# rospy.loginfo("Check next position")
-
-		# topic for the laserScans og kinect
+		# topic for the laserScans of kinect
 		distance_infront = rospy.Subscriber('/scan', LaserScan, self.distance_from_obstacle_ahead)
 
-		# rospy.loginfo(self.infront_distance)
-
-		# If there is an odstacle 2.5m ahead, you should stop moving
+		# If there is an odstacle 1.5m ahead, you should stop moving
 		if math.isnan(self.infront_distance) == False and self.infront_distance < 1.5: 
 			return False
 		else: # else, free space continue straight
@@ -73,17 +63,6 @@ class GoForward(): # go straight until you reach an odstacle
 
 	def distance_from_obstacle_ahead(self, data): # find my current position
 		self.infront_distance = data.ranges[320]
-
-
-	def get_rotation(self, msg): # find where I am looking at
-		global roll, pitch, yaw
-		orientation_q = msg.pose.pose.orientation
-		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-		(roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-		# yaw euler
-		self.degrees = math.degrees(yaw) # degrees
-		# print degrees
-
  
 if __name__ == '__main__':
     try:
